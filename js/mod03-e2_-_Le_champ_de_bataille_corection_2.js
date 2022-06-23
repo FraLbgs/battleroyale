2c331f7996940429a33a3eba1eab182ccede2480
Franck Lebourgeois <franck.lebourgeois@gmail.com>
2022-06-23 21:35:06 +0200

    put characters on fight zone on click + add button

--- js/mod03-e2_-_Le_champ_de_bataille_corection_2.js
+++ js/mod03-e2_-_Le_champ_de_bataille_corection_2.js
@@ -1,4 +1,5 @@
 let characters = [];
+let fighters = [];
 
 try{
     fetch("https://akabab.github.io/superhero-api/api/all.json")
@@ -20,128 +21,116 @@ catch (error) {
 //     console.log(publisher);
 // }
 
-let charList = "";
 
 function getCharacters(arr){
+    let charList = "";
     for(const char of arr){
-        characters.push(char.name);
-    }
-    characters.sort();
-    const infoChar = {};
-    for(const char of arr){
+        const infoChar = {};
         infoChar.name=char.name;
-        infoChar.img=char.images.xs;
+        infoChar["img-xs"]=char.images.xs;
+        infoChar["img-md"]=char.images.md;
         infoChar.stats={
             weapon:parseInt((char.powerstats.intelligence+char.powerstats.strength)/2),
             shield:char.powerstats.durability,
             combat:char.powerstats.combat,
-            life:100
+            life:50
         }
-        charList += "<div><img src='"+infoChar.img+"' alt='"+infoChar.name+"'> " +infoChar.name+ " </div>";
+        // console.log(infoChar);
+        characters.push(infoChar);
+        // console.log(characters);
+        charList += "<div data-char='"+characters.indexOf(infoChar)+"'><img src='"+infoChar["img-xs"]+"' alt='"+infoChar.name+"'> " +infoChar.name+ " </div>";
     }
     document.getElementById("characters").innerHTML = charList;
+
+    const charList2 = document.querySelectorAll(".characters div");
+    
+    for(const char of charList2){
+        const stats = document.createElement("ul");
+        let li = "";
+        char.addEventListener("click", function(e){
+            fighters.push(characters[char.dataset.char]);
+            const divCharStats = document.createElement("div");
+            divCharStats.appendChild(char);
+            document.getElementById("fighters").appendChild(divCharStats);
+            // console.log(characters[char.dataset.char].stats);
+            for(const s in characters[char.dataset.char].stats){
+                li += "<li>"+s+" : "+characters[char.dataset.char].stats[s]+"</li>"
+            }
+            console.log(stats);
+            stats.innerHTML += li;
+            // document.getElementById("fighters").innerHTML += "<div>"+stats+"</div>";
+            divCharStats.appendChild(stats);
+
+
+            console.table(fighters);
+        });
+    }
 }
 
 
-
-
-
-
-
-//  let characters = [
-//     {
-//         name: "King Kong",
-//         life: 50,
-//         xp: 6,
-//         weapon: 5,
-//         shield: 2
-//     },
-//     {
-//         name: "Josephine AG",
-//         life: 50,
-//         xp: 8,
-//         weapon: 1,
-//         shield: 2
-//     },
-//     {
-//         name: "Naruto",
-//         life: 50,
-//         xp: 3,
-//         weapon: 8,
-//         shield: 3
-//     },
-//     {
-//         name: "Spiderman",
-//         life: 50,
-//         xp: 7,
-//         weapon: 5,
-//         shield: 5
-//     },
-// ];
-
-
-
-// function getRandomValue(max) {
-//     return parseInt(Math.random()*max);
-// }
+function getRandomValue(max) {
+    return parseInt(Math.random() * max);
+}
 
 // // Séléctionner aléatoirement un joueur
-// function getRandomChar(notThisOne) {
-//     const c = characters[getRandomValue(characters.length)];
-//     if (c !== notThisOne) return c;
-//     return getRandomChar(notThisOne);
-// }
+function getRandomChar(notThisOne) {
+    const c = fighters[getRandomValue(fighters.length)];
+    if (c !== notThisOne) return c;
+    return getRandomChar(notThisOne);
+}
 
 // // Récupérer le score d'attaque d'un joueur
-// function getAttackScore(char) {
-//     return char.xp + getRandomValue(char.weapon);
-// }
+function getAttackScore(char) {
+    return char.stats.weapon + getRandomValue(char.stats.combat);
+}
 
 // // Récupérer le score de défense d'un joueur
-// function getDefenseScore(char) {
-//     return char.xp + getRandomValue(char.shield);
-// }
+function getDefenseScore(char) {
+    return char.stats.shield + getRandomValue(char.stats.combat);
+}
 
 // // Baisser les points de vie d'un joueur
-// function decreaseLife(char, value) {
-//     char.life -= value;
-//     return char.life;
-// }
+function decreaseLife(char, value) {
+    char.stats.life -= value;
+    return char.stats.life;
+}
 
 // // Sortir un joueur mort du jeu
-// function buryTheDeads() {
-//     characters = characters.filter(char => char.life > 0);
-// }
+function buryTheDeads() {
+    fighters = fighters.filter(char => char.stats.life > 0);
+}
 
 // // Faire s'affronter 2 joueurs
-// function fight(a, d) {
-//     const attackScore = getAttackScore(a);
-//     if (attackScore > getDefenseScore(d)) {
-//         decreaseLife(d, attackScore);
-//         buryTheDeads();
-//         return true;
-//     }
-//     return false;
-// }
+function fight(a, d) {
+    const attackScore = getAttackScore(a);
+    const defScore = getDefenseScore(d);
+    console.log(`${a.name} avec une attaque de ${attackScore} fonce sur ${d.name} qui a une défense de ${defScore}.`);
+    if (attackScore > defScore) {
+        decreaseLife(d, attackScore-defScore);
+        console.log(`${d.name} perd ${attackScore-defScore} points de vie, il lui en reste ${d.stats.life} !`); 
+        buryTheDeads();
+        return true;
+    }
+    console.log(`${d.name} résiste à l'attaque !`);
+    return false;
+}
 
-// function battle() {
-//     const attacker = getRandomChar();
-//     const defender = getRandomChar(attacker);
-//     console.log(`${attacker.name} is attacking ${defender.name}.`);
+function battle() {
+    const attacker = getRandomChar();
+    const defender = getRandomChar(attacker);
     
-//     const resultText = fight(attacker, defender) ? "win" : "lose";
-//     console.log(`${attacker.name} ${resultText}.`);
+    fight(attacker, defender);
     
-//     console.table(characters);
+    for(const f of fighters){
+        console.table(f.name+" PV restants : "+f.stats.life);
+    }
     
-//     if (characters.length <= 1) {
-//         console.table(characters);
-//         console.log(`The winner is ${characters[0].name}.`);
-//         return;
-//     }
-//     return battle();
-// }
+    if (fighters.length <= 1) {
+        console.table(fighters);
+        console.log(`The winner is ${fighters[0].name}.`);
+        return;
+    }
+    return; //battle();
+}
 
-// battle();
-
-// // console.log(attacker, defender);
+document.getElementById("fight").addEventListener("click", battle);
