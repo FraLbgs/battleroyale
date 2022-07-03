61a06e52398ac49c4a28576d4a3075a7ec4c9ade
Franck Lebourgeois <franck.lebourgeois@gmail.com>
2022-07-03 13:14:19 +0200

    add local storage + cimetery

--- js/mod03-e2_-_Le_champ_de_bataille_corection_2.js
+++ js/mod03-e2_-_Le_champ_de_bataille_corection_2.js
@@ -23,7 +23,7 @@ catch (error) {
 // }
 
 
-function setStats(str, obj){
+function setStats(obj){
     str = "<a href='#' title='Weapon : "+obj.stats.weapon+
     "\nShield : "+obj.stats.shield+
     "\nCombat : "+obj.stats.combat+
@@ -33,52 +33,64 @@ function setStats(str, obj){
     return str;
 }
 
+
 function getCharacters(arr){
-    let charList = "";
-    let stats = "";
-    for(const char of arr){
-        const infoChar = {};
-        infoChar.name=char.name;
-        infoChar.img_xs=char.images.xs;
-        infoChar.img_sm=char.images.sm;
-        infoChar.stats={
-            weapon:parseInt((char.powerstats.intelligence+char.powerstats.strength)/2),
-            shield:char.powerstats.durability,
-            combat:char.powerstats.combat,
-            life:50
+    if(localStorage.getItem("chars") !== null){
+        try{
+            document.getElementById("characters").innerHTML = JSON.parse(localStorage.getItem("chars"));
+            characters = JSON.parse(localStorage.getItem("characters"));
+            document.getElementById("cimetery").innerHTML = JSON.parse(localStorage.getItem("cimetery"));
+
         }
-        characters.push(infoChar);
-        stats += setStats(charList, infoChar);
+        catch(e){console.log(e,'error');}
+
     }
-    document.getElementById("characters").innerHTML = stats;
-
-    const charTab = document.getElementById("characters");
-    charTab.addEventListener("click", function(e){
-        if(e.target.className === "characters") return;
-        const stats = document.createElement("ul");
-        stats.className = "stats-list";
-        let li = "";
-        const cible = e.target.hasAttribute("data-char") ? e.target : e.target.parentElement;
-        const index = cible.dataset.char;
-        fighters.push(characters[index]);
-        cible.firstElementChild.src = characters[index].img_sm;
-        const divCharStats = document.createElement("div");
-        divCharStats.className = "char-stat";
-        divCharStats.appendChild(cible);
-        const keys = Object.keys(characters[index].stats);
-        const values = Object.values(characters[index].stats);
-        console.log(keys, values);
-        for(let i=0;i<3;i++){
-            li += "<li>"+keys[i]+" : "+values[i]+"</li>"
+    else{
+        let stats = "";
+        for(const char of arr){
+            const infoChar = {};
+            infoChar.name=char.name;
+            infoChar.img_xs=char.images.xs;
+            infoChar.img_sm=char.images.sm;
+            infoChar.stats={
+                weapon:parseInt((char.powerstats.intelligence+char.powerstats.strength)/2),
+                shield:char.powerstats.durability,
+                combat:char.powerstats.combat,
+                life:50
+            }
+            characters.push(infoChar);
+            stats += setStats(infoChar);
         }
-        li += "<li>"+keys[3]+" : <meter class='pv' max='50' low='20' value="+values[3]+"></meter></li>"
-        stats.innerHTML += li;
-        divCharStats.appendChild(stats);
-        document.getElementById("fighters").appendChild(divCharStats);
+        document.getElementById("characters").innerHTML = stats;
+    }
 
-        console.table(fighters);
-    });
-
+        const charTab = document.getElementById("characters");
+        charTab.addEventListener("click", function(e){
+            if(e.target.className === "characters") return;
+            const stats = document.createElement("ul");
+            stats.className = "stats-list";
+            let li = "";
+            const cible = e.target.hasAttribute("data-char") ? e.target : e.target.parentElement;
+            const index = cible.dataset.char;
+            fighters.push(characters[index]);
+            // console.log(index, characters[index]);
+            // console.log(characters);
+            cible.firstElementChild.src = characters[index].img_sm;
+            const divCharStats = document.createElement("div");
+            divCharStats.className = "char-stat";
+            divCharStats.appendChild(cible);
+            const keys = Object.keys(characters[index].stats);
+            const values = Object.values(characters[index].stats);
+            for(let i=0;i<3;i++){
+                li += "<li>"+keys[i]+" : "+values[i]+"</li>"
+            }
+            li += "<li>"+keys[3]+" : <meter class='pv' max='50' low='20' value="+values[3]+"></meter></li>"
+            stats.innerHTML += li;
+            divCharStats.appendChild(stats);
+            document.getElementById("fighters").appendChild(divCharStats);
+    
+            // console.table(fighters);
+        });
 }
 
 
@@ -115,9 +127,11 @@ function decreaseLife(char, value) {
 function buryTheDeads() {
     for(const fighter of fighters){
         if(fighter.stats.life<=0){
-            console.log(fighter.stats.life);
-            document.querySelector('[data-char="'+characters.indexOf(fighter)+'"]').parentElement.remove();
+            // console.log(document.querySelector('[data-char="'+characters.indexOf(fighter)+'"]'));
+            document.getElementById("cimetery").appendChild(document.querySelector('[data-char="'+characters.indexOf(fighter)+'"]').parentElement);
+            // document.querySelector('[data-char="'+characters.indexOf(fighter)+'"]').parentElement.remove();
             details.innerHTML += `${fighter.name} succombe à l'attaque !!!<br><br>`;
+            // characters.splice([characters.indexOf(fighter)], 1);
         }
     }
     fighters = fighters.filter(char => char.stats.life > 0);
@@ -136,40 +150,41 @@ function fight(a, d) {
     
     
     // Open the modal
-    console.log(modal);
+    // console.log(modal);
     modal.style.display = "block"; 
 
     document.getElementById('imgFightersAtt').src = a.img_sm;
     document.getElementById('imgFightersDef').src = d.img_sm;
     details.innerHTML = `${a.name} avec une attaque de ${attackScore} fonce sur ${d.name} qui a une défense de ${defScore}.<br><br>`;
-    console.log(`${a.name} avec une attaque de ${attackScore} fonce sur ${d.name} qui a une défense de ${defScore}.`);
+    // console.log(`${a.name} avec une attaque de ${attackScore} fonce sur ${d.name} qui a une défense de ${defScore}.`);
     if (attackScore > defScore) {
         decreaseLife(d, attackScore-defScore);
         details.innerHTML += `${d.name} perd ${attackScore-defScore} points de vie, `;
         if(d.stats.life>0){
             details.innerHTML += `il lui en reste ${d.stats.life} !<br><br>`;
         }
-        console.log(`${d.name} perd ${attackScore-defScore} points de vie, il lui en reste ${d.stats.life} !`); 
+        // console.log(`${d.name} perd ${attackScore-defScore} points de vie, il lui en reste ${d.stats.life} !`); 
         buryTheDeads();
         return true;
     }
     details.innerHTML += `${d.name} résiste à l'attaque !<br><br>`;
-    console.log(`${d.name} résiste à l'attaque !`);
+    // console.log(`${d.name} résiste à l'attaque !`);
     return false;
 }
 
 function battle() {
+    console.log("test");
     const attacker = getRandomChar();
     const defender = getRandomChar(attacker);
     
     fight(attacker, defender);
     
     for(const f of fighters){
-        console.table(f.name+" PV restants : "+f.stats.life);
+        // console.table(f.name+" PV restants : "+f.stats.life);
     }
     
     if (fighters.length <= 1) {
-        console.table(fighters);
+        // console.table(fighters);
         details.innerHTML += `Le gagnant est ${fighters[0].name} avec ${fighters[0].stats.life} PV restant.`;
         console.log(`The winner is ${fighters[0].name}.`);
         let link = document.querySelector(".fighters a");
@@ -199,7 +214,7 @@ document.getElementById("new-select").addEventListener("click", function(e){
     document.getElementById("myModal").style.display="none";
     const battlefield = document.getElementById("fighters");
     battlefield.firstElementChild.remove();
-    battlefield.innerHTML = setStats(battlefield.innerHTML, fighters[0]);
+    battlefield.innerHTML = setStats(fighters[0]);
     const link = battlefield.firstElementChild;
     link.classList.remove("winner-pic");
     link.firstElementChild.src = characters[link.dataset.char].img_xs;
@@ -208,4 +223,22 @@ document.getElementById("new-select").addEventListener("click", function(e){
     fighters = []
     document.getElementById("new-select").style.display="none";
     document.getElementById("fight").style.display="block";
+    try {
+        localStorage.setItem("chars", JSON.stringify(document.getElementById("characters").innerHTML));
+        localStorage.setItem("characters", JSON.stringify(characters));
+        localStorage.setItem("cimetery", JSON.stringify(document.getElementById("cimetery").innerHTML));
+      }
+    catch(e){console.log(e, "error");}
+});
+
+document.getElementById("reset").addEventListener("click", function(e){
+    localStorage.removeItem("chars");
+    localStorage.removeItem("cimetery");
+    document.location.reload();
+});
+
+document.getElementById("cimetery-btn").addEventListener("click", function(e){
+    // console.log(document.getElementById("cimetery"));
+    document.getElementById("cimetery").classList.toggle("cim-display");
+    document.getElementById("main-fight").classList.toggle("main-fight-display");
 });
